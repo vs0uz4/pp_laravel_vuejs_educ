5dfecb1b3d583978c3fca68314aa330baa58c9fa
Vitor S Rodrigues <vitor.rodrigues@gmail.com>
2017-07-16 19:54:46 -0300

    Override SendResetPassword method in UserModel and Implement My Custom Notification for ResetPassword

--- app/User.php
+++ app/User.php
@@ -4,6 +4,7 @@ namespace SiGeEdu;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use SiGeEdu\Notifications\MyOwnResetPassword as ResetPasswordNotification;
 
 class User extends Authenticatable
 {
@@ -26,4 +27,16 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Send the password reset notification.
+     *
+     * @param  string  $token
+     * @return void
+     */
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new ResetPasswordNotification($token));
+    }
+
 }
